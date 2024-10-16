bbd8b6138c45305ba3f6668aa200b78a72fab0cd
Fabio <51410334+theenergici@users.noreply.github.com>
2024-10-16 16:28:09 +0200

    Added movement to player
    created Movement for player, with North/south speed and East/West speed, the press of 2 buttons at the same time will have the middle direction applied.
    The folder Utils was created and VectorMath has the function rotate Vector in 2d.

--- Assets/scripts/PlayerMonobehaviour.cs
+++ Assets/scripts/PlayerMonobehaviour.cs
@@ -1,20 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+
 
 public class PlayerMonobehaviour : MonoBehaviour
 {
-    public float speed = 5;
+
+        
+    private const float MaxSpeed = 25.0f;
+    [Range(0.0f, MaxSpeed)]
+    public float NS_Speed = 5.0f;
+    [Range(0.0f, MaxSpeed)]
+    public float EW_Speed = 5.0f;
+    private Vector2 inputMove;
+    private Vector2 scalingVector;
+    
+    private Rigidbody2D rb;
     
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        scalingVector = new Vector2(EW_Speed, NS_Speed);
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        rb.MovePosition(rb.position + Time.fixedDeltaTime*inputMove*scalingVector);
     }
+
+    public void OnMovement(InputValue value){
+
+        Vector2 t = value.Get<Vector2>();
+        inputMove = VectorMath.rotateVector(t, -Mathf.PI/4);
+    }
+
+
+
 }
