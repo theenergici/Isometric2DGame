8249ffa09c4c46b6746dac0c4fd23133bfb5b03f
Fabio <51410334+theenergici@users.noreply.github.com>
2024-10-19 20:52:42 +0200

    Task 2 map creation (#14)
    * Update sample scene
    
    now tile map in sample scene has different layers per each level
    
    * added hitboxes to map, and created tiles
    
    map now has hitboxes and created tile object later going to be used for pathing algorithm,

--- Assets/scripts/MapManager.cs
+++ Assets/scripts/MapManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
@@ -11,6 +12,10 @@ public class MapManager : MonoBehaviour
             return _instance;    
         }
     }
+    [SerializeField]
+    Tile tilePrefab;
+
+    public Dictionary<Vector2, Tile> map;
     
     private void Awake() {
         if(_instance!= null && _instance!= this)
@@ -18,8 +23,39 @@ public class MapManager : MonoBehaviour
 
         else _instance = this;
         
+        
+
+    }
+
+    private void Start() {
+        GameObject TileContainer= new GameObject();
+        TileContainer.transform.parent = this.transform;
+        TileContainer.name = "TileContainer";
         var tilemap = GetComponentInChildren<Tilemap>();
+        map = new Dictionary<Vector2, Tile>();
         BoundsInt bounds = tilemap.cellBounds;
 
+        for(int z= bounds.zMax; z> bounds.zMin; z--){
+
+            for(int x = bounds.xMin; x< bounds.xMax; x++){
+
+                for(int y = bounds.yMin; y< bounds.yMax; y++){
+
+                    var tileLoc = new Vector3Int(x,y,z);
+                    var tileKey = new Vector2(x,y);
+
+                    if(tilemap.HasTile(tileLoc) && !map.ContainsKey(tileKey) ){
+
+                        var tempTile = Instantiate(tilePrefab, TileContainer.transform);
+                        var cellWorldPosition = tilemap.GetCellCenterWorld(tileLoc);
+                        tempTile.transform.position = new Vector3(cellWorldPosition.x,cellWorldPosition.y,cellWorldPosition.z + 1);
+                        // tempTile.GetComponent<SpriteRenderer>().sortingOrder = tilemap.GetComponent<TilemapRenderer>().sortingOrder;
+
+                        map.Add(tileKey, tempTile);
+
+                    }
+                }
+            }
+        }
     }
 }
